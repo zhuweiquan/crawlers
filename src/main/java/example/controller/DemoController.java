26e95fefb49964b8315a6a11a55a770b6a8b9c15
weiquan <865807189@qq.com>
2018-09-10 01:35:31 +0800

    基础spring框架搭建，并且测试成功

---
+++ src/main/java/example/controller/DemoController.java
@@ -0,0 +1,16 @@
+package example.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class DemoController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
+
