4b25691367072431b00e2b3eb4dfd1f826185578
weiquan <865807189@qq.com>
2018-09-11 01:27:52 +0800

    配置日志log4文件

--- src/main/java/example/controller/DemoController.java
+++ src/main/java/example/controller/DemoController.java
@@ -3,13 +3,21 @@ package example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.log4j.Logger;
 
 @Controller
 @RequestMapping("/home")
 public class DemoController {
+    private static Logger logger = Logger.getLogger(DemoController.class);
 
     @RequestMapping("/index")
     public String index() {
+        String s = "ab";
+        int k = 11;
+        System.out.println(s.hashCode());
+        logger.info("eee");
+
+
         return "index";
     }
 }
---
+++ src/main/webapp/views/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhu
+  Date: 2018/9/10
+  Time: 1:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<style type="text/css">
+    h1{
+        font-family: "Times New Roman";
+    }
+</style>
+<body>
+    <h1>Kings Landing!!!</h1>
+    <p style="font-family: 楷体;font-size: 20px;">翻页</p>
+</body>
+</html>
